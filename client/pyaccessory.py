e2d5fdca39163a5792cf7d4ccb15049431d3946e
Arn-O <rnd.bertrand@gmail.com>
2016-03-13 23:32:33 +0100

    test device on interface type #1

--- client/pyaccessory.py
+++ client/pyaccessory.py
@@ -14,15 +14,8 @@ import sys
 import time
 import random
 
-VID_GALAXY_NEXUS_DEBUG = 0x04e8
-PID_GALAXY_NEXUS_DEBUG = 0x6860
-
 VID_ANDROID_ACCESSORY = 0x18d1
 PID_ANDROID_ACCESSORY = 0x2d01
-
-# To develop :
-#   - remove Accessory mode ?
-#   - start application when already in accessory mode
 
 def get_accessory():
     print('Looking for Android Accessory')
@@ -33,75 +26,73 @@ def get_accessory():
     return dev
 
 def get_android_device():
-    print('Looking for Galaxy Nexus')
-    print('VID: 0x%0.4x - PID: 0x%0.4x'
-        % (VID_GALAXY_NEXUS_DEBUG, PID_GALAXY_NEXUS_DEBUG))
-    android_dev = usb.core.find(idVendor=VID_GALAXY_NEXUS_DEBUG, 
-        idProduct=PID_GALAXY_NEXUS_DEBUG)
-    if android_dev:
-        print('Samsung Galaxy Nexus (debug) found')
+    """Look for a potential Android device"""
+    print "Looking for an Android device"
+    ldev = usb.core.find(bDeviceClass=0)
+    if ldev:
+        print "Device found"
+        print "VID: {:#04x} PID: {:#04x}".format(
+            ldev.idVendor,
+            ldev.idProduct
+        )
     else:
-        sys.exit('No Android device found')
-    return android_dev
-
+        sys.exit("No Android device found")
+    return ldev
 
 def set_protocol(ldev):
-
-    try:
-        ldev.set_configuration()
-    except usb.core.USBError as e:
-        if  e.errno == 16:
-            print('Device already configured, should be OK')
-        else:
-            sys.exit('Configuration failed')
-
+    """Set the USB configuration"""
+    #try:
+    ldev.set_configuration()
+    #except usb.core.USBError as e:
+    #if  e.errno == 16:
+    #        print('Device already configured, should be OK')
+    #    else:
+    #        sys.exit('Configuration failed')
     ret = ldev.ctrl_transfer(0xC0, 51, 0, 0, 2)
-
-# Dunno how to translate: array('B', [2, 0])
+    print ret
     protocol = ret[0]
-
     print('Protocol version: %i' % protocol)
     if protocol < 2:
         sys.exit('Android Open Accessory protocol v2 not supported')
-
     return
 
 def set_strings(ldev):
+    """Send series of strings to activate accessory mode"""
     send_string(ldev, 0, 'Arn-O')
     send_string(ldev, 1, 'PyAndroidAccessory')
     send_string(ldev, 2, 'A Python based Android accessory')
     send_string(ldev, 3, '0.1.0-beta')
     send_string(ldev, 4, 
             'https://github.com/Arn-O/py-android-accessory/')
-    #send_string(ldev, 5, '2254711SerialNo.')    
     return
 
 def set_accessory_mode(ldev):
+    """Trigger the accessory mode"""
     ret = ldev.ctrl_transfer(0x40, 53, 0, 0, '', 0)    
-    if ret:
-        sys.exit('Start-up failed')
+    assert not ret
     time.sleep(1)
     return
 
 def send_string(ldev, str_id, str_val):
+    """Send a give string to the Android device"""
     ret = ldev.ctrl_transfer(0x40, 52, 0, str_id, str_val, 0)
-    if ret != len(str_val):
-        sys.exit('Failed to send string %i' % str_id) 
+    assert ret == len(str_val)
     return 
 
 def start_accessory_mode():
+    """Start the accessory mode on the Android device"""
+    #dev = get_accessory()
+    #if not dev:
+    #print('Android accessory not found')
+    #print('Try to start accessory mode')
+    dev = get_android_device()
+    #set_protocol(dev)
+    set_strings(dev)
+    set_accessory_mode(dev)
     dev = get_accessory()
-    if not dev:
-        print('Android accessory not found')
-        print('Try to start accessory mode')
-        dev = get_android_device()
-        set_protocol(dev)
-        set_strings(dev)
-        set_accessory_mode(dev)
-        dev = get_accessory()
-        if not dev:
-            sys.exit('Unable to start accessory mode')
-    print('Accessory mode started')
+    #if not dev:
+        #sys.exit('Unable to start accessory mode')
+    #print('Accessory mode started')
     return dev
 
 def sensor_variation(toss):
@@ -166,7 +157,9 @@ def wait_for_command(ldev):
 def main():
     """Where everything starts"""
     dev = start_accessory_mode()
-    wait_for_command(dev)
+    if dev:
+        print "Accessory mode started"
+    #wait_for_command(dev)
 
 if __name__ == '__main__':
     main()
